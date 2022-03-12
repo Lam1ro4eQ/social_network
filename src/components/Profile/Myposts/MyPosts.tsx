ef059572a1b2a2a837a4d7129cda70b1c0d58f68
Lam1ro4eQ <alsajgushev@gmail.com>
2022-03-12 14:32:41 +0300

    dispatch action

--- src/components/Profile/Myposts/MyPosts.tsx
+++ src/components/Profile/Myposts/MyPosts.tsx
@@ -16,11 +16,13 @@ const MyPosts = (props: PropsType) => {
 
     let addNewTextArea = (e: ChangeEvent<HTMLTextAreaElement>) => {
             let text = e.currentTarget.value;
-            props.addNewText(text);
+           // props.addNewText(text);
+        props.dispatch(type: 'ADD-NEW-TEXT', newText: text)
     }
 
     let addPost = () => {
-        props.addPost();
+        //props.addPost();
+        props.dispatch(type: 'ADD-POST')
     }
 
     let postsMap = props.profilePage.postsData.map(post => <Post message={post.message} likesCount={post.likesCount}/>) //мапим данные для отрисовки
