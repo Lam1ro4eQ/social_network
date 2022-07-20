ac6d320026959e79952077ab00f72ab881f52b57
Lam1ro4eQ <alsajgushev@gmail.com>
2022-07-20 18:47:14 +0300

    ts lesson

---
+++ src/lessons/state.ts
@@ -0,0 +1,24 @@
+let age = 10;
+let firstName = 'Dimych';
+let isMentor = true;
+
+let ages: Array<number> = [18, 12, 19, 22, 45];
+let ages1: number[] = [18, 12, 19, 22, 45];
+type AgesType = Array<number>
+
+
+let arr: Array<number | string | boolean> = [18, '12', 21, true]
+let arr2: (number | string | boolean)[] = [18, '12', 21, true]
+
+let man1: { name: string, height: number } = {name: 'Dima', height: 1.78}
+let man2 = {name: 'Sasha', height: 1.8}
+let car = {model: 'Reno Stepway', year: 2016}
+type ManType = { name: string, height: number }
+
+
+
+
+
+
+
+export default 15;
