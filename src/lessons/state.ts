e3a3a00ee611cb88364fb7c55861c13337b13ea5
Lam1ro4eQ <alsajgushev@gmail.com>
2022-07-22 19:22:57 +0300

    typescript

--- src/lessons/state.ts
+++ src/lessons/state.ts
@@ -1,22 +1,51 @@
-let age = 10;
-let firstName = 'Dimych';
-let isMentor = true;
+// let age = 10;
+// let firstName = 'Dimych';
+// let isMentor = true;
+//
+// let ages: Array<number> = [18, 12, 19, 22, 45];
+// let ages1: number[] = [18, 12, 19, 22, 45];
+// type AgesType = Array<number>
+//
+//
+// let arr: Array<number | string | boolean> = [18, '12', 21, true]
+// let arr2: (number | string | boolean)[] = [18, '12', 21, true]
+//
+//
+// let man1: IMan = {name: 'Dima', height: 1.78}
+// let man2: ManType = {name: 'Sasha', height: 1.8}
+// let car: ICar = {model: 'Reno Stepway', year: 2016}
+// type ManType = { name: string, height: number }
+// interface IMan {
+//     name: string,
+//     height: number
+// }
+// interface ICar {
+//     model: string,
+//     year: number
+// }
 
-let ages: Array<number> = [18, 12, 19, 22, 45];
-let ages1: number[] = [18, 12, 19, 22, 45];
-type AgesType = Array<number>
+
+// let mans: Array<MansType | Cartype> = [
+//     {name: 'Dima', height: 1.78, surname: 'Safronov'},
+//     {name: 'Sasha', height: 1.8},
+//     {model: 'Reno Stepway', year: 2016}
+// ]
+// type MansType = {
+//     name: string
+//     height: number
+//     surname?: string
+// }
+// type Cartype = {
+//     model: string,
+//     year: number
+// }
 
 
-let arr: Array<number | string | boolean> = [18, '12', 21, true]
-let arr2: (number | string | boolean)[] = [18, '12', 21, true]
 
-let man1: { name: string, height: number } = {name: 'Dima', height: 1.78}
-let man2 = {name: 'Sasha', height: 1.8}
-let car = {model: 'Reno Stepway', year: 2016}
-type ManType = { name: string, height: number }
-
-
-
+function toUpperCase(strings: Array<string>): Array<string>{
+    let result = strings.map(s => s.toUpperCase());
+    return result;
+}
 
 
 
