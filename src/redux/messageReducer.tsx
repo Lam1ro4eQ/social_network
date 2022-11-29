c5d121f5aaa4607a59e05e67c5e890e9dee26ef9
Lam1ro4eQ <alsajgushev@gmail.com>
2022-11-29 17:13:12 +0300

    ls48 deep & shallow copyState

--- src/redux/messageReducer.tsx
+++ src/redux/messageReducer.tsx
@@ -1,7 +1,10 @@
 import state, {MessagePageType, MessagesDataType, PostsDataType, rootActionType, StateType} from "./store";
+
 const ADD_MESSAGE = "ADD-MESSAGE"
 const ADD_NEW_TEXT_MESSAGE = "ADD-NEW-TEXT-MESSAGE"
-export type messageActionType = ReturnType<typeof addMessageActionCreator> | ReturnType<typeof addNewTextMessageActionCreator>
+export type messageActionType =
+    ReturnType<typeof addMessageActionCreator>
+    | ReturnType<typeof addNewTextMessageActionCreator>
 
 
 let initialState = {
@@ -20,26 +23,34 @@ let initialState = {
 }
 
 
-
-const messageReducer = (state: MessagePageType = initialState,action: messageActionType) => {
+const messageReducer = (state: MessagePageType = initialState, action: messageActionType) => {
 
     switch (action.type) {
-        case ADD_MESSAGE:
+        case ADD_MESSAGE: {
             let newMessage: MessagesDataType = {
                 id: 4,
                 message: state.newMessageState
             };
-            state.messagesData.push(newMessage);
-            state.newMessageState = "";
+            let stateCopy = {...state}
+            stateCopy.messagesData = [...state.messagesData]
+            stateCopy.messagesData.push(newMessage);
+            stateCopy.newMessageState = "";
+            return stateCopy;
+        }
+        case ADD_NEW_TEXT_MESSAGE: {
+            let stateCopy = {...state}
+            stateCopy.newMessageState = action.newTextMessage;
+            return stateCopy;
+        }
+        default:
             return state;
-        case ADD_NEW_TEXT_MESSAGE:
-            state.newMessageState = action.newTextMessage;
-            return state;
-        default: return state;
     }
 
 }
 export const addMessageActionCreator = () => ({type: ADD_MESSAGE} as const)
-export const addNewTextMessageActionCreator = (textMessage:string) => ({type: ADD_NEW_TEXT_MESSAGE, newTextMessage: textMessage} as const)
+export const addNewTextMessageActionCreator = (textMessage: string) => ({
+    type: ADD_NEW_TEXT_MESSAGE,
+    newTextMessage: textMessage
+} as const)
 
 export default messageReducer;
