447109a74662444ddcff8e788f32379735d2390d
Lam1ro4eQ <alsajgushev@gmail.com>
2023-01-28 13:20:54 +0300

    54lesson social-network

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -3,7 +3,7 @@ import {connect} from "react-redux";
 import {followAC, InitialStateType, setUsersAC, unfollowAC, UserLocationType, UserType} from "../../redux/usersReducer";
 import {Dispatch} from "redux";
 import {AppStateType} from "../../redux/redux-store";
-import Users from "./UsersC";
+import Users from "./Users";
 
 type MapStatePropsType = {
     usersPage: InitialStateType
