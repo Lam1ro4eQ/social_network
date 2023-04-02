6d2620ec1f87523273c7f82a5300104ef0222a76
Lam1ro4eQ <alsajgushev@gmail.com>
2023-04-02 12:34:12 +0300

    pagination

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {followAC, InitialStateType, setUsersAC, unfollowAC, UserLocationType, UserType} from "../../redux/usersReducer";
+import {
+    followAC,
+    InitialStateType,
+    setCurrentPageAC,
+    setUsersAC,
+    unfollowAC,
+    UserLocationType,
+    UserType
+} from "../../redux/usersReducer";
 import {Dispatch} from "redux";
 import {AppStateType} from "../../redux/redux-store";
 import Users from "./Users";
 
 type MapStatePropsType = {
     usersPage: InitialStateType
+    pageSize: number
+    totalUsersCount: number
+    currentPage: number
 }
 
 export type MapDispatchPropsType = {
     follow: (userId: number) => void
     unfollow: (userId: number) => void
     setUsers: (users:Array<UserType>) => void
+    setCurrentPage: (pageNumber:number) => void
 }
 
 export type UsersPropsType = MapStatePropsType & MapDispatchPropsType
 
 let mapStateToProps = (state:AppStateType):MapStatePropsType => {
     return {
-        usersPage: state.usersPage
+        usersPage: state.usersPage,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 
@@ -33,6 +48,9 @@ let mapDispatchToProps = (dispatch: Dispatch):MapDispatchPropsType => {
         },
         setUsers: (users:Array<UserType>) => {
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage: (pageNumber:number) => {
+            dispatch(setCurrentPageAC(pageNumber))
         }
     }
 }
--- src/redux/usersReducer.tsx
+++ src/redux/usersReducer.tsx
@@ -1,11 +1,10 @@
-
-
 const FOLLOW = "FOLLOW"
 const UNFOLLOW = "UNFOLLOW"
 const SET_USERS = "SETUSERS"
+const SET_CURRENT_PAGE = "SETCURRENTPAGE"
 
 export type usersActionType =
-    ReturnType<typeof followAC> | ReturnType<typeof unfollowAC> | ReturnType<typeof setUsersAC>
+    ReturnType<typeof followAC> | ReturnType<typeof unfollowAC> | ReturnType<typeof setUsersAC> | ReturnType<typeof setCurrentPageAC>
 
 
 export type UserLocationType = {
@@ -22,24 +21,30 @@ export type UserType = {
     location: UserLocationType
 }
 export type PhotosType = {
-    small:string
-    large:string
+    small: string
+    large: string
 }
 
 export type InitialStateType = {
     users: Array<UserType>
+    pageSize: number
+    totalUsersCount: number
+    currentPage: number
 }
 
 let initialState: InitialStateType = {
-    users: []
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 21,
+    currentPage: 4
 }
 
-const usersReducer = (state: InitialStateType = initialState, action: usersActionType):InitialStateType => {
+const usersReducer = (state: InitialStateType = initialState, action: usersActionType): InitialStateType => {
 
     switch (action.type) {
         case FOLLOW:
             return {
-                ...state, users:state.users.map(u => {
+                ...state, users: state.users.map(u => {
                     if (u.id === action.userId) {
                         return {...u, followed: true}
                     }
@@ -47,27 +52,31 @@ const usersReducer = (state: InitialStateType = initialState, action: usersActio
                 })
             }
         case UNFOLLOW:
-                    return {
-                        ...state, users:state.users.map(u => {
-                            if (u.id === action.userId) {
-                                return {...u, followed: false}
-                            }
-                            return u;
-                        })
-                    };
+            return {
+                ...state, users: state.users.map(u => {
+                    if (u.id === action.userId) {
+                        return {...u, followed: false}
+                    }
+                    return u;
+                })
+            };
         case SET_USERS:
-            return {...state, users: [...state.users, ...action.users]}
+            return {...state, users: [ ...action.users]};
+
+        case SET_CURRENT_PAGE:
+            return {...state, currentPage: action.currentPage};
 
         default:
             return state;
 
-}
+    }
 
 
 }
-export const followAC = (userId:number) => ({type: FOLLOW, userId} as const)
-export const unfollowAC = (userId:number) => ({type: UNFOLLOW, userId} as const)
-export const setUsersAC = (users:Array<UserType>) => ({type: SET_USERS, users} as const)
+export const followAC = (userId: number) => ({type: FOLLOW, userId} as const)
+export const unfollowAC = (userId: number) => ({type: UNFOLLOW, userId} as const)
+export const setUsersAC = (users: Array<UserType>) => ({type: SET_USERS, users} as const)
+export const setCurrentPageAC = (currentPage: number) => ({type: SET_CURRENT_PAGE, currentPage} as const)
 
 export default usersReducer;
 
