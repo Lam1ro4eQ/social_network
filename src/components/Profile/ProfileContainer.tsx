a297cbfbe176e8bb4314cac251abf245f3d4bc7f
Lam1ro4eQ <alsajgushev@gmail.com>
2023-04-18 14:14:58 +0300

    error

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,44 +1,40 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Profile from "./Profile";
 import axios from "axios";
 import {connect} from "react-redux";
 import {ProfileType, setUserProfile} from "../../redux/profileReducer";
 import {AppStateType} from "../../redux/redux-store";
-import {RouteComponentProps, withRouter} from "react-router-dom";
+import {useParams} from 'react-router-dom';
 
-
-type WithRouterType = {
-    userId: string
-}
-
-type PropsType = RouteComponentProps<WithRouterType> &  ProfileApiContainerType
 
 type MapStatePropsType = {
     profile: ProfileType | null
 }
 
 type MapDispatchPropsType = {
-    setUserProfile: (profile:ProfileType) => void
+    setUserProfile: (profile: ProfileType) => void
 }
 
 export type ProfileApiContainerType = MapDispatchPropsType & MapStatePropsType
 
-class ProfileApiContainer extends React.Component<PropsType> {
 
-    componentDidMount() {
-        let userId = this.props.match.params.userId
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/`+ userId).then(response => {
-            this.props.setUserProfile(response.data)
+const ProfileApiContainer = (props: ProfileApiContainerType) => {
+    let userId = useParams();
+    useEffect(()=>{
+
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId).then(response => {
+            props.setUserProfile(response.data)
         });
-    }
+    },[])
 
-    render() {
-        return (
-            <div>
-                <Profile {...this.props}/>
-            </div>
-        )
-    }
+
+
+    return (
+        <div>
+            <Profile {...props}/>
+        </div>
+    )
+
 }
 
 let mapStateToProps = (state: AppStateType): MapStatePropsType => ({
@@ -46,6 +42,7 @@ let mapStateToProps = (state: AppStateType): MapStatePropsType => ({
 });
 
 
-let WithUrlDataComponent = withRouter(ProfileApiContainer)
+// let WithUrlDataComponent = WithRouter(ProfileApiContainer)
 
-export const ProfileContainer = connect(mapStateToProps, {setUserProfile})(WithUrlDataComponent)
+
+export const ProfileContainer = connect(mapStateToProps, {setUserProfile})(ProfileApiContainer)
